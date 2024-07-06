4164671fb1ceab2f379517c57b298478eed0d637
taishi0514 <notaishi4@gmail.com>
2024-07-06 23:51:23 +0900

    ログイン機能追加

---
+++ spring_project/src/main/java/com/example/spring_project/controller/LoginController.java
@@ -0,0 +1,48 @@
+package com.example.spring_project.controller;
+
+import org.springframework.security.web.WebAttributes;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.spring_project.constant.UrlConst;
+import com.example.spring_project.form.LoginForm;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class LoginController {
+
+	/** セッション情報 */
+	private final HttpSession session;
+
+	/**
+	 * 初期表示
+	 * 
+	 * @param model モデル
+	 * @param form 入力情報
+	 * @return 表示画面
+	 */
+	@GetMapping(UrlConst.LOGIN)
+	public String view(Model model, LoginForm form) {
+		return "login";
+	}
+
+	/**
+	 * ログインエラー画面表示
+	 * 
+	 * @param model モデル
+	 * @param form 入力情報
+	 * @return 表示画面
+	 */
+
+	@GetMapping(value = UrlConst.LOGIN, params = "error")
+	public String viewWithError(Model model, LoginForm form) {
+		var errorInfo = (Exception) session.getAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+		model.addAttribute("errorMsg", errorInfo.getMessage());
+		return "login";
+	}
+
+}
